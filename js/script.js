a5f24aea99918fa96f78b482878863e862e3ddab
Milad TAKJOO <milad.takjoo@gmail.com>
2024-07-28 23:48:55 +0330

    responsive

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
 $(".carousel1").mouseover(function(){
     $("#infCardCrousel1").css('display','initial');
-    $("#infCardCrousel1").css('animation-name','topbardropdown');
-    $("#infCardCrousel1").css('animation-duration','0.8s');
-    $("#infCardCrousel1").css('animation-fill-mode','forwards');
+    $("#infCardCrousel1").css('animation','1s forwards topbardropdown');
 });
 $(".carousel1").mouseleave(function(){
-    $("#infCardCrousel1").css('animation-name','out');
-    $("#infCardCrousel1").css('animation-duration','0.8s');
-    $("#infCardCrousel1").css('animation-fill-mode','forwards');
     $("#infCardCrousel1").css('display','none');
 });
 $(".carousel2").mouseover(function(){
+    $("#infCardCrousel2").css('animation','1s forwards topbardropdown');
     $("#infCardCrousel2").css('display','initial');
-    $("#infCardCrousel2").css('animation-name','topbardropdown');
-    $("#infCardCrousel2").css('animation-duration','0.8s');
-    $("#infCardCrousel2").css('animation-fill-mode','forwards');
 });
 $(".carousel2").mouseleave(function(){
-    $("#infCardCrousel2").css('animation-name','out');
-    $("#infCardCrousel2").css('animation-duration','0.8s');
-    $("#infCardCrousel2").css('animation-fill-mode','forwards');
     $("#infCardCrousel2").css('display','none');
 });
 $(".carousel3").mouseover(function(){
+    $("#infCardCrousel3").css('animation','1s forwards topbardropdown');
     $("#infCardCrousel3").css('display','initial');
-    $("#infCardCrousel3").css('animation-name','topbardropdown');
-    $("#infCardCrousel3").css('animation-duration','0.8s');
-    $("#infCardCrousel3").css('animation-fill-mode','forwards');
 });
 $(".carousel3").mouseleave(function(){
-    $("#infCardCrousel3").css('animation-name','out');
-    $("#infCardCrousel3").css('animation-duration','0.8s');
-    $("#infCardCrousel3").css('animation-fill-mode','forwards');
     $("#infCardCrousel3").css('display','none');
 });
+
+let elements=['.carousel1 img:nth-child(2)','.carousel1 img:nth-child(3)','.carousel1 img:nth-child(4)','.carousel1 img:nth-child(5)'];
+let srcs=['dist/images/carousel/nioh2.jpg',"dist/images/carousel/nioh3.jpg","dist/images/carousel/nioh4.jpg","dist/images/carousel/nioh5.jpg"];
+for(var i=0;i<4;i++){
+$(elements[i]).mouseover(function(){
+    $('.carousel1 img:nth-child(1)').attr('src',srcs[i]);
+    console.log('aa')
+})
+$(elements[i]).mouseleave(function(){
+    $('.carousel1 img:nth-child(1)').attr('src',"dist/images/carousel/nioh1.jpg");
+})
+}
