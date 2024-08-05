5d2a62200d7ee154c016864be6575bd061f47bd6
Milad TAKJOO <milad.takjoo@gmail.com>
2024-08-05 23:37:18 +0330

    halfFinal

--- js/script.js
+++ js/script.js
@@ -130,3 +130,11 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
